3a6a8123638a931468f0d3f4dcf2b592e8805ae3
YuzuruS <navitima@gmail.com>
2016-06-01 23:34:00 +0900

    画像取得

--- getrss.rb
+++ getrss.rb
@@ -8,14 +8,14 @@ require 'open-uri'
 require 'nokogiri'
 require 'openssl'
 require 'pp'
-require 'FileUtils'
+require 'fileutils'
+require 'digest/md5'
 
 OpenSSL::SSL::VERIFY_PEER = OpenSSL::SSL::VERIFY_NONE
 
 config = YAML.load_file('database.yml')
 ActiveRecord::Base.establish_connection(config['db']['development'])
 class Item < ActiveRecord::Base
-  # バリデーションの記述など
   validates_presence_of :title
   validates_presence_of :link
   validates_uniqueness_of :link
@@ -41,8 +41,6 @@ class ManageRss
           rssdata.items.each do |entry|
             tmpdate = entry.respond_to?(:pubDate) ? entry.pubDate : entry.dc_date
             item = Item.new
-            pp entry
-            exit
             item.title = entry.title
             item.link = entry.link
             item.description = entry.description
@@ -51,8 +49,7 @@ class ManageRss
             insertData << item
           end
         rescue => e
-          p e
-            # エラー時の処理
+          pp e
         end
       end
     end
@@ -66,13 +63,33 @@ class ManageRss
     def getImage(url)
       charset = nil
       html = open(url, @@opt) do |f|
-        charset = f.charset # 文字種別を取得
-        f.read # htmlを読み込んで変数htmlに渡す
+        charset = f.charset
+        f.read
       end
-      # ノコギリを使ってhtmlを解析
+
       doc = Nokogiri::HTML.parse(html, charset)
       imgUrl = doc.css('//meta[property="og:image"]/@content').to_s
 
+      fileExt = File.extname(imgUrl)
+      if fileExt.empty?
+        return ''
+      end
+
+      fileNameWithoutExt = File.basename(imgUrl, fileExt)
+      fileName =  Digest::MD5.hexdigest(Time.now.usec.to_s + fileNameWithoutExt).to_s + fileExt
+      dirName = File.dirname(__FILE__) + '/img/' + Time.now.strftime("%Y/%m/").to_s
+      relativePath = 'img/' + Time.now.strftime("%Y/%m/").to_s + fileName
+      filePath = dirName + fileName
+
+      # create folder if not exist
+      FileUtils.mkdir_p(dirName) unless FileTest.exist?(dirName)
+
+      open(filePath, 'wb') do |output|
+        open(imgUrl) do |data|
+          output.write(data.read)
+        end
+      end
+      return relativePath
     end
 end
 
